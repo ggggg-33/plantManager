c2939c0fa6410ae0d6d2366c971e2b5239710cf0
ggggg-33 <ivantvi2004@gmail.com>
2025-11-18 12:42:52 +0300

    Update Controller.java

--- src/main/java/org/example/plantmanager/ui/Controller.java
+++ src/main/java/org/example/plantmanager/ui/Controller.java
@@ -179,11 +179,6 @@ public class Controller {
             return new SimpleStringProperty(date != null ? date.format(dateFormatter) : "—");
         });
 
-          TableColumn<Plant, String> plantingDateCol = new TableColumn<>("Дата роста");
-        plantingDateCol.setCellValueFactory(cellData -> {
-            LocalDate date = cellData.getValue().getPlantingDate();
-            return new SimpleStringProperty(date != null ? date.format(dateFormatter) : "—");
-        });
 
         TableColumn<Plant, String> plantingDateCol = new TableColumn<>("Дата посадки");
         plantingDateCol.setCellValueFactory(cellData -> {
